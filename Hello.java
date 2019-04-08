dad7b0f603e5d398fdfe8d2fa8c4decf06b1b03f
Gaurav Maskara <Gaurav.Maskara@in.ey.com>
2019-04-08 13:31:52 +0530

    First commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+
+
+
+class Hello {
+   public static void main(String args[]){
+    	System.out.println("Jenkins successfully Integrated 1");
+       System.out.println("Jenkins successfully Integrated 2");
+   }
+}
